999daa0086d66315a6d57c5069e92c80c5de9aaa
GavinGu <guweixigavin@gmail.com>
2015-06-10 20:36:53 +0800

    修改bug: 1.从用户信息验证页直接跳至个人平台控制页。 2.任务完成后转跳至个人页面没有信息的错误问题。 3. 修改了两个数据表xmfb1, xmyj1

--- src/main/java/com/xthena/sckf/manager/JyXmYjManager.java
+++ src/main/java/com/xthena/sckf/manager/JyXmYjManager.java
@@ -3,6 +3,7 @@ package com.xthena.sckf.manager;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.xthena.sckf.domain.JyXm;
 import com.xthena.sckf.domain.JyXmYj;
 import com.xthena.security.util.SpringSecurityUtils;
 import com.xthena.bpm.persistence.domain.BpmProcess;
@@ -13,6 +14,7 @@ import com.xthena.gcgl.manager.PjXmManager;
 
 import org.activiti.engine.IdentityService;
 import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.RuntimeService;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,18 +22,35 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class JyXmYjManager extends HibernateEntityDao<JyXmYj> {
-
-	
 	@Autowired
 	private PjXmManager pjXmManager;
-
 	@Autowired
 	private ProcessEngine processEngine;
-	
-	
 	@Autowired
 	private BpmProcessManager bpmProcessManager;
-	
+
+    @Transactional
+    public void newXmyj(JyXmYj jyXmYj){
+
+            BpmProcess bpmProcess = bpmProcessManager.findUniqueBy("name", "项目移交");
+            String processDefinitionId = bpmProcess.getBpmConfBase()
+                    .getProcessDefinitionId();
+
+            IdentityService identityService = processEngine.getIdentityService();
+            identityService.setAuthenticatedUserId(SpringSecurityUtils
+                    .getCurrentUserId());
+
+            Map<String, Object> processParameters = new HashMap<String, Object>();
+            //jyXmYj.setFmemo4("已申请");
+            save(jyXmYj);
+
+            ProcessInstance processInstance = processEngine.getRuntimeService()
+                    .startProcessInstanceById(processDefinitionId, jyXmYj.getFid().toString(),
+                            processParameters);
+            jyXmYj.setFtaskid(processInstance.getProcessInstanceId());
+            save(jyXmYj);
+    }
+
 	@Transactional
 	public void saveForYj(JyXmYj dest) {
 	    BpmProcess bpmProcess = bpmProcessManager.findUniqueBy("name", "项目移交");
@@ -41,20 +60,15 @@ public class JyXmYjManager extends HibernateEntityDao<JyXmYj> {
         IdentityService identityService = processEngine.getIdentityService();
         identityService.setAuthenticatedUserId(SpringSecurityUtils
                 .getCurrentUserId());
-        
         Map<String, Object> processParameters = new HashMap<String, Object>();
         dest.setFyijiaostatus("已申请");
         save(dest);
-        
-        ProcessInstance processInstance = processEngine.getRuntimeService()
-                .startProcessInstanceById(processDefinitionId, dest.getFid().toString(),
+        String dest_id=dest.getFid().toString();
+        RuntimeService runtimeService=processEngine.getRuntimeService();
+        ProcessInstance processInstance = runtimeService.startProcessInstanceById(processDefinitionId, dest_id,
                         processParameters);
         dest.setFtaskid(processInstance.getProcessInstanceId());
     	save(dest);
-		
 	}
-	
-	
-	
 
 }
