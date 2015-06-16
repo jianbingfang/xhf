ccefeb3bdef70e5f50a3cbde37c603948494eba5
GavinGu <guweixigavin@gmail.com>
2015-06-16 13:28:50 +0800

    证件info

---
+++ src/main/java/com/xthena/hr/manager/HrZhengjianinfoManager.java
@@ -0,0 +1,9 @@
+package com.xthena.hr.manager;
+
+import com.xthena.core.hibernate.HibernateEntityDao;
+import com.xthena.hr.domain.HrZhengjianinfoEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HrZhengjianinfoManager extends HibernateEntityDao<HrZhengjianinfoEntity> {
+}
